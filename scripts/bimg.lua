b33ec1e2d70da5df194b418e95b8a1cbaa697bdb
teassy000 <6188980+teassy000@users.noreply.github.com>
2024-11-25 20:35:39 +0800

    Update bimg.lua
    update bimg lib to support all functions

--- scripts/bimg.lua
+++ scripts/bimg.lua
@@ -11,25 +11,44 @@ project "bimg"
 	files
 	{
 		path.join(BIMG_DIR, "include/bimg/*.h"),
-		path.join(BIMG_DIR, "src/image.cpp"),
-		path.join(BIMG_DIR, "src/image_gnf.cpp"),
-		path.join(BIMG_DIR, "src/*.h"),
-		path.join(BIMG_DIR, "3rdparty/astc-codec/src/decoder/*.cc"),
-		
-		path.join(BIMG_DIR, "3rdparty/astc-encoder/source/**.cpp"),
-		path.join(BIMG_DIR, "3rdparty/astc-encoder/source/**.h"),
+		path.join(BIMG_DIR, "src/**"),
+
+		path.join(BIMG_DIR, "3rdparty/**"),
 	}
+
 	includedirs
 	{
 		path.join(BX_DIR, "include"),
 		path.join(BIMG_DIR, "include"),
-		path.join(BIMG_DIR, "3rdparty/astc-codec"),
-		path.join(BIMG_DIR, "3rdparty/astc-codec/include"),
+		path.join(BIMG_DIR, "3rdparty"),
 
+		-- astc-encoder
 		path.join(BIMG_DIR, "3rdparty/astc-encoder/include"),
+		-- edtaa3
+		path.join(BIMG_DIR, "3rdparty/edtaa3"),
+		-- etc1
+		path.join(BIMG_DIR, "3rdparty/etc1"),
+		-- etc2
+		path.join(BIMG_DIR, "3rdparty/etc2"),
+		-- iqa
+		path.join(BIMG_DIR, "3rdparty/iqa/include"),
+		-- libsquish
+		path.join(BIMG_DIR, "3rdparty/libsquish"),
+		-- lodepng
+		path.join(BIMG_DIR, "3rdparty/lodepng"),
+		-- nvtt
+		path.join(BIMG_DIR, "3rdparty/nvtt"),
+		-- pvrtc
+		path.join(BIMG_DIR, "3rdparty/pvrtc"),
+		-- stb
+		path.join(BIMG_DIR, "3rdparty/stb"),
+		-- tinyexr
+		path.join(BIMG_DIR, "3rdparty/tinyexr/deps/miniz"),
+		path.join(BIMG_DIR, "3rdparty/tinyexr"),
 	}
 
 	using_bx()
 	set_bx_compat()
 	enable_msvc_muiltithreaded()
 	disable_msvc_crt_warning()
+	common_filter()
