b176ac7b5824bef06fe68debda890725dfb30875
teassy000 <6188980+teassy000@users.noreply.github.com>
2024-12-19 18:22:17 +0800

    add new implement of smaa

---
+++ src/shaders/smaa_impl.h
@@ -0,0 +1,16 @@
+// the following smaa implement based on following project:
+// https://github.com/turol/smaaDemo
+// 
+// Copyright and License
+// =====================
+// 
+// Copyright (c) 2015-2024 Alternative Games Ltd / Turo Lamminen
+// 
+// This code is licensed under the MIT license (see license.txt).
+// 
+// Third-party code under foreign/ is licensed according to their respective licenses.
+
+
+#define SMAA_GLSL_4 1
+
+#include "smaa.h"
